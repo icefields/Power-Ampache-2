71bb2eca8aeb4b2984d7e605f482b22486d9ed1e
icefields <arctic.icefields@slmails.com>
2024-01-11 17:31:59 -0500

    sorting playlists by rating

--- app/src/main/java/luci/sixsixsix/powerampache2/data/local/MusicDao.kt
+++ app/src/main/java/luci/sixsixsix/powerampache2/data/local/MusicDao.kt
@@ -100,7 +100,7 @@ interface MusicDao {
     @Query("DELETE FROM playlistentity")
     suspend fun clearPlaylists()
 
-    @Query("""SELECT * FROM playlistentity WHERE LOWER(name) LIKE '%' || LOWER(:query) || '%' OR LOWER(:query) == name""")
+    @Query("""SELECT * FROM playlistentity WHERE LOWER(name) LIKE '%' || LOWER(:query) || '%' OR LOWER(:query) == name order by rating DESC""")
     suspend fun searchPlaylists(query: String): List<PlaylistEntity>
 
     //@Query("""DELETE FROM playlistentity artistentity, songentity, albumentity""")
