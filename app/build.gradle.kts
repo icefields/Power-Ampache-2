23a463be937541a7fe37d671212332b008065886
icefields <arctic.icefields@slmails.com>
2024-01-23 11:13:15 -0500

    updated libraries in gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,11 +10,11 @@ plugins {
 }
 
 val composeVersion = "1.5.4" // rootProject.extra.get("compose_version") as String
+val lifecycleVersion = "2.7.0"
 val retrofit2Version = "2.9.0" //rootProject.extra.get("retrofit2_version") as String
 val coroutinesVersion = "1.7.3" //rootProject.extra.get("coroutines_version") as String
 val exoplayerVersion = "2.19.1" // rootProject.extra.get("exoplayer_version") as String
 val composeNavVersion = "1.8.42-beta" // rootProject.extra.get("composeNav_version") as String
-
 val media3Version = "1.2.1"
 
 val localProperties = Properties()
@@ -94,45 +94,45 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
     implementation("androidx.activity:activity-compose:1.8.2")
-
     implementation("androidx.media:media:1.7.0")
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
 
-    // Compose dependencies
+    // --- Compose --- //
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.material:material-icons-extended:1.5.4")
     implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
     implementation("androidx.compose.material3:material3:1.1.2")
     implementation("androidx.compose.ui:ui-graphics:$composeVersion")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
     implementation("com.google.accompanist:accompanist-flowlayout:0.17.0")
     implementation("androidx.paging:paging-compose:3.3.0-alpha02")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("com.google.accompanist:accompanist-swiperefresh:0.24.2-alpha")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
     // DO NOT INCLUDE implementation("androidx.compose.material:material:$composeVersion")
 
-    // Compose Nav Destinations
-    // implementation("io.github.raamcosta.compose-destinations:core:$composeNavVersion")
+    // --- Compose Nav-Destinations --- //
     // Version with animations
     implementation("io.github.raamcosta.compose-destinations:animations-core:$composeNavVersion")
+    // version with no animations
+    // implementation("io.github.raamcosta.compose-destinations:core:$composeNavVersion")
     ksp("io.github.raamcosta.compose-destinations:ksp:$composeNavVersion")
 
-    // coroutines in Android
+    // COROUTINES
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:$coroutinesVersion")
 
-    // ExoPlayer
+    // --- ExoPlayer --- //
     implementation("androidx.media3:media3-exoplayer:$media3Version")
-    // DASH playback support with ExoPlayer
-    // implementation("androidx.media3:media3-exoplayer-dash:$media3Version")
     // HLS playback support with ExoPlayer
     implementation("androidx.media3:media3-exoplayer-hls:$media3Version")
+    // DASH playback support with ExoPlayer
+    // implementation("androidx.media3:media3-exoplayer-dash:$media3Version")
     // RTSP playback support with ExoPlayer
     // implementation("androidx.media3:media3-exoplayer-rtsp:$media3Version")
-
     // Common functionality for media database components
     implementation("androidx.media3:media3-database:$media3Version")
     // Common functionality for media decoders
@@ -144,17 +144,17 @@ dependencies {
     implementation("androidx.media3:media3-session:$media3Version")
     implementation("androidx.media3:media3-ui:$media3Version")
 
-    // Coil
+    // --- Coil --- //
     implementation("io.coil-kt:coil-compose:2.4.0")
 
-    //Dagger - Hilt
+    // --- Dagger Hilt --- //
     implementation("com.google.dagger:hilt-android:2.48")
     kapt("com.google.dagger:hilt-android-compiler:2.48")
     kapt("androidx.hilt:hilt-compiler:1.1.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
     //implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
 
-    // Retrofit
+    // --- Retrofit --- //
     implementation("com.squareup.retrofit2:retrofit:$retrofit2Version")
     //implementation("com.squareup.retrofit2:converter-moshi:$retrofit2Version")
     implementation("com.squareup.retrofit2:converter-gson:$retrofit2Version")
@@ -163,12 +163,13 @@ dependencies {
     // JSON serialization
     implementation("com.google.code.gson:gson:2.10")
 
-    // Room
+    // --- Room --- //
     implementation("androidx.room:room-runtime:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
     // Kotlin Extensions and Coroutines support for Room
     implementation("androidx.room:room-ktx:2.6.1")
 
+    // --- TESTING --- //
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
