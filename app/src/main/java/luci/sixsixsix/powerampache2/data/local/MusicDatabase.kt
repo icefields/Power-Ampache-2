8bc1e99bef8a5b9bd4e1559c90389008fc0f7342
icefields <arctic.icefields@slmails.com>
2024-01-26 16:30:58 -0500

    added database migration

--- app/src/main/java/luci/sixsixsix/powerampache2/data/local/MusicDatabase.kt
+++ app/src/main/java/luci/sixsixsix/powerampache2/data/local/MusicDatabase.kt
@@ -3,6 +3,9 @@ package luci.sixsixsix.powerampache2.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+import luci.sixsixsix.mrlog.L
 import luci.sixsixsix.powerampache2.data.local.entities.AlbumEntity
 import luci.sixsixsix.powerampache2.data.local.entities.ArtistEntity
 import luci.sixsixsix.powerampache2.data.local.entities.CredentialsEntity
@@ -24,9 +27,15 @@ import luci.sixsixsix.powerampache2.data.local.entities.UserEntity
         UserEntity::class,
         LocalSettingsEntity::class,
         DownloadedSongEntity::class
-    ], version = 72
+    ], version = 73
 )
 @TypeConverters(Converters::class)
 abstract class MusicDatabase: RoomDatabase() {
     abstract val dao: MusicDao
 }
+
+fun MIGRATION_73_74() =
+    Migration(73, 74) { database: SupportSQLiteDatabase ->
+        // do nothing if not altering tables.
+        L.e("MIGRATING DATABASE")
+    }
