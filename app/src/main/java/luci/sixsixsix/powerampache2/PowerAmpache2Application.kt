e7a183dfbfcb1836477d367a5c89cbd7e11d9fc6
icefields <arctic.icefields@slmails.com>
2024-01-29 21:24:44 -0500

    added license, new icons

--- app/src/main/java/luci/sixsixsix/powerampache2/PowerAmpache2Application.kt
+++ app/src/main/java/luci/sixsixsix/powerampache2/PowerAmpache2Application.kt
@@ -22,6 +22,25 @@ import org.acra.data.StringFormat
 import org.acra.ktx.initAcra
 import javax.inject.Inject
 
+/**
+ *
+ * LICENSE: GNU Affero General Public License, version 3 (AGPL-3.0-or-later)
+ * Copyright Ampache.org, 2001-2023
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
 @HiltAndroidApp
 class PowerAmpache2Application : Application(), ImageLoaderFactory, Configuration.Provider {
 
