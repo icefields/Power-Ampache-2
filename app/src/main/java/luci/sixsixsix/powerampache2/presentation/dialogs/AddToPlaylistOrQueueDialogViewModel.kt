4d547656943d8d38cb7e37dd459849788422a09e
Ivan 'ztx/lyghters' Sivkov <lyghters@gmail.com>
2026-06-17 04:13:22 +0500

    AddToPlaylistOrQueueDialog: fix playlist filtering Change the obsolete playlist filtering logic. Additionally, filter out any smartlists.

--- app/src/main/java/luci/sixsixsix/powerampache2/presentation/dialogs/AddToPlaylistOrQueueDialogViewModel.kt
+++ app/src/main/java/luci/sixsixsix/powerampache2/presentation/dialogs/AddToPlaylistOrQueueDialogViewModel.kt
@@ -41,8 +41,8 @@ import luci.sixsixsix.powerampache2.domain.errors.ErrorHandler
 import luci.sixsixsix.powerampache2.domain.models.Playlist
 import luci.sixsixsix.powerampache2.domain.models.PlaylistType
 import luci.sixsixsix.powerampache2.domain.models.Song
-import luci.sixsixsix.powerampache2.domain.models.USER_SYSTEM
 import luci.sixsixsix.powerampache2.domain.models.User
+import luci.sixsixsix.powerampache2.domain.models.isSmartPlaylist
 import luci.sixsixsix.powerampache2.domain.usecase.UserFlowUseCase
 import luci.sixsixsix.powerampache2.domain.usecase.playlists.PlaylistsFlow
 import luci.sixsixsix.powerampache2.domain.usecase.playlists.PlaylistsUseCase
@@ -114,19 +114,15 @@ class AddToPlaylistOrQueueDialogViewModel @Inject constructor(
     }
 
     private fun filterPlaylists(playlists: List<Playlist>, user: User): List<Playlist> {
-        val username = user.username.lowercase()
+        val filteredList = mutableListOf<Playlist>()
 
-        return if (user.isAdmin()) {
-            playlists.sortedBy { it.name }.sortedBy { playlist ->
-                when (playlist.owner?.lowercase()) {
-                    username -> 0
-                    USER_SYSTEM -> 1
-                    else -> 2
-                }
+        playlists.filterNot { it.isSmartPlaylist() }.forEach { playlist ->
+            if (user.isAdmin() || user.username.equals(other = playlist.owner, ignoreCase = true)) {
+                filteredList.add(playlist)
             }
-        } else {
-            playlists.filter { it.owner?.lowercase() == username }.sortedBy { it.name }
         }
+
+        return filteredList
     }
 
     private fun createPlaylistAddSong(
